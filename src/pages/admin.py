82043cce29ccea7117d3da72f40a7e850b16b1ea
Jorge Morgado <jorge.morgadov@gmail.com>
2021-05-01 14:20:55 -0400

    Add new models

--- src/pages/admin.py
+++ src/pages/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import *
+
 # Register your models here.
+
+admin.site.register(Battle)
+admin.site.register(BattleParticipant)
+admin.site.register(Attack)
+admin.site.register(Ent)
+admin.site.register(Spell)
+admin.site.register(BattleEvent)
 admin.site.register(Player)
-admin.site.register(Beast)
--- src/pages/models.py
+++ src/pages/models.py
@@ -1,12 +1,45 @@
 from django.db import models
 
 # Create your models here.
+class Battle(models.Model):
+    place = models.CharField(max_length=50, blank=False, null=False)
+    date = models.DateField(blank=False, null=False)
+
+class BattleParticipant(models.Model):
+    ent = models.ForeignKey("Ent", models.CASCADE)
+    battle = models.ForeignKey("Battle", models.CASCADE)
+    winner = models.BooleanField(default=False)
+
+class Attack(models.Model):
+    defender_pts_before = models.IntegerField(blank=False, null=False)
+    spell = models.ForeignKey("Spell", models.CASCADE)
+
+class Ent(models.Model):
+    name = models.CharField(max_length=50, blank=False, null=False)
+    raze = models.CharField(max_length=50, blank=False, null=False)
+    damage = models.CharField(max_length=50, blank=False, null=False)
+    weakness = models.CharField(max_length=50, blank=False, null=False)
+    health = models.IntegerField(default=100, blank=False, null=False)
+    attacks = models.ManyToManyField("Ent", through=Attack)
+    battles = models.ManyToManyField(Battle, through=BattleParticipant)
+
+class Spell(models.Model):
+    name = models.CharField(max_length=50, blank=False, null=False)
+    damage = models.CharField(max_length=50, blank=False, null=False)
+    average_pts = models.IntegerField()
+
+class BattleEvent(models.Model):
+    attacker = models.ForeignKey(Ent, models.CASCADE, related_name='attacker')
+    defender = models.ForeignKey(Ent, models.CASCADE, related_name='defender')
+    battle = models.ForeignKey(Battle, models.CASCADE)
+    number = models.IntegerField(blank=False, null=False)
+
 class Player(models.Model):
-    name = models.TextField()
-    raze = models.TextField()
-    damage = models.IntegerField()
-    weakness = models.TextField()
+    ent = models.OneToOneField(Ent, models.CASCADE)
+    spells_in_use = models.ManyToManyField(Spell, related_name='in_use')
+    known_spells = models.ManyToManyField(Spell, related_name='knonw')
 
 class Beast(models.Model):
-    name = models.TextField()
-    raze = models.TextField()
+    ent = models.OneToOneField(Ent, models.CASCADE)
+    damage_pts = models.IntegerField()
+
